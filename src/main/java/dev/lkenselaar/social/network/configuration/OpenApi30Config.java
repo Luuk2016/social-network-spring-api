fd1c8a62110754ce2c44c25e71b6db17d9283083
Luuk Kenselaar <luuk@lkenselaar.dev>
2022-10-12 00:12:19 +0200

    Update OpenAPIDefinition

--- src/main/java/dev/lkenselaar/social/network/configuration/OpenApi30Config.java
+++ src/main/java/dev/lkenselaar/social/network/configuration/OpenApi30Config.java
@@ -2,8 +2,11 @@ package dev.lkenselaar.social.network.configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -11,12 +14,27 @@ import org.springframework.context.annotation.Configuration;
  * @since 15/06/2022
  */
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Social network API", version = "v0.1"))
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Social network API",
+        version = "v0.1",
+        contact = @Contact(
+            name = "Luuk Kenselaar",
+            url = "https://lkenselaar.dev/",
+            email = "luuk@lkenselaar.dev"
+        ),
+        license = @License(
+            name = "MIT Licence",
+            url = "https://github.com/Luuk2016/social-network-spring-api/blob/master/LICENSE")),
+        servers = @Server(url = "http://localhost:8080")
+)
+
 @SecurityScheme(
     name = "bearerAuth",
     type = SecuritySchemeType.HTTP,
     bearerFormat = "JWT",
     scheme = "bearer"
 )
+
 public class OpenApi30Config {
 }
